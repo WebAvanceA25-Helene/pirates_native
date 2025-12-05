a993d8a813435e0bae4d7b22ba1bea5430628d37
Hélène Caron <2355372@cegeprdl.ca>
2025-12-05 16:57:30 -0500

    Ajout des fonctionnalités ajouter et retirer de l'or

--- views/boatCard.tsx
+++ views/boatCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 import CheckBox from "expo-checkbox";
 import { useState } from "react";
 import { Boat } from "../types/boatType";
@@ -16,6 +16,7 @@ const base_url = "https://edwrdlhelene.me:2222/api"
 
 const BoatCard = ({ boat, setErrorMessage, getBoats }: Props) => {
   const [isChecked, setIsChecked] = useState(false);
+  const [goldQty, setGoldQty] = useState(0);
 
   const handleCheckBoxCheck = () => {
     setIsChecked(!isChecked);
@@ -32,7 +33,7 @@ const BoatCard = ({ boat, setErrorMessage, getBoats }: Props) => {
           'Authorization': 'Bearer ' + token
         },
         body: JSON.stringify({
-          id: boat.id
+          oldCrewSize: boat.crewSize
         })
       }); //Helped by AI (existence du fetch et ajout des headers et du stringify)
 
@@ -40,7 +41,7 @@ const BoatCard = ({ boat, setErrorMessage, getBoats }: Props) => {
         getBoats();
         setErrorMessage("Crew member hiring successful");
       } else {
-        setErrorMessage("Crew member hiring unsuccessful");
+        setErrorMessage("Crew member hiring unsuccessful" +res.status+ res.statusText);
       }
       
     } catch (res) {
@@ -59,7 +60,7 @@ const BoatCard = ({ boat, setErrorMessage, getBoats }: Props) => {
           'Authorization': 'Bearer ' + token
         },
         body: JSON.stringify({
-          id: boat.id
+          oldCrewSize: boat.crewSize
         })
       }); //Helped by AI (existence du fetch et ajout des headers et du stringify)
 
@@ -67,11 +68,67 @@ const BoatCard = ({ boat, setErrorMessage, getBoats }: Props) => {
         getBoats();
         setErrorMessage("Crew member firing successful");
       } else {
-        setErrorMessage("Crew member firing unsuccessful");
+        setErrorMessage("Crew member firing unsuccessful"+res.statusText);
       }
       
     } catch (res) {
       setErrorMessage("Failed to fire crew member from this boat\t" + res);
+    }
+  }
+
+  const handleAddGold = async () => {
+    try {
+      const token = await SecureStore.getItemAsync('token');
+      
+      const res = await fetch(base_url + '/ships/addGold/'+boat.id, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify({
+          oldGoldQty: boat.goldCargo,
+          goldQty: goldQty
+        })
+      }); //Helped by AI (existence du fetch et ajout des headers et du stringify)
+
+      if(res.ok) {
+        getBoats();
+        setErrorMessage("Gold adding successful");
+      } else {
+        setErrorMessage("Gold adding unsuccessful" + res.statusText);
+      }
+      
+    } catch (res) {
+      setErrorMessage("Failed to add gold on this boat\t" + res);
+    }
+  }
+
+  const handleTakeOutGold = async () => {
+    try {
+      const token = await SecureStore.getItemAsync('token');
+      
+      const res = await fetch(base_url + '/ships/takeOutGold/'+boat.id, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify({
+          oldGoldQty: boat.goldCargo,
+          goldQty: goldQty
+        })
+      }); //Helped by AI (existence du fetch et ajout des headers et du stringify)
+
+      if(res.ok) {
+        getBoats();
+        setErrorMessage("Gold taking out successful");
+      } else {
+        setErrorMessage("Gold taking out unsuccessful" + res.statusText);
+      }
+      
+    } catch (res) {
+      setErrorMessage("Failed to take gold from this boat\t" + res);
     }
   }
 
@@ -92,14 +149,18 @@ const BoatCard = ({ boat, setErrorMessage, getBoats }: Props) => {
       {/* Main details */}
       <View style={{ gap: 4 }}>
         <Text>Captain: {boat.captain}</Text>
-        <Text>Gold cargo: {boat.goldCargo}</Text>
-        <Text>Crew size: {boat.crewSize} <Button title="+" onPress={handleIncrementCrewSize}></Button><Button title="-" onPress={handleDecrementCrewSize}></Button></Text>
+        <Text>Gold cargo: {boat.goldCargo}
+          <Button title="-" onPress={handleTakeOutGold}/>
+          <TextInput keyboardType="numeric" onChangeText={(number) => setGoldQty(Number(number))} value={String(goldQty)} style={styles.input}/>
+          <Button title="+" onPress={handleAddGold}/>
+        </Text>
+        <Text>Crew size: {boat.crewSize} <Button title="-" onPress={handleDecrementCrewSize}/><Button title="+" onPress={handleIncrementCrewSize}/></Text>
         <Text>Status: {boat.status}</Text>
         <Text>Created by: {boat.createdBy}</Text>
       </View>
     </View>
   );
-};
+};//Transtypage vient de l'AI
 
 const styles = StyleSheet.create({
 
@@ -125,6 +186,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 6,
     elevation: 4,
+  },
+  input: {
+    width: "40%",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
   }
 })
 
